666a90196cdfcae030dc261df7261e82ee3aa395
链盟 <lianmeng@lianmengdeMacBook-Pro-2.local>
2018-02-23 14:57:45 +0800

    finish

--- app.js
+++ app.js
@@ -9,24 +9,24 @@ app.set("view engine", 'ejs')
 app.set('views', __dirname + '/views')
 
 app.get('/', (req, res) => {
-	res.render('index')
+	res.render('index_backup')
 })
 
-app.get('/case/:id', (req, res, next) => {
-	let { id } = req.params
-	const data = caseData.filter(x => x.id === id )
-	if( data.length ) {
-		const itemData = data[0];
-		res.render('detail', {
-			id:'asasda',
-			title: itemData.title,
-			desc: itemData.desc,
-			imgs: itemData.imgs
-		})
-	} else {
-		next()
-	}
-})
+// app.get('/case/:id', (req, res, next) => {
+// 	let { id } = req.params
+// 	const data = caseData.filter(x => x.id === id )
+// 	if( data.length ) {
+// 		const itemData = data[0];
+// 		res.render('detail', {
+// 			id:'asasda',
+// 			title: itemData.title,
+// 			desc: itemData.desc,
+// 			imgs: itemData.imgs
+// 		})
+// 	} else {
+// 		next()
+// 	}
+// })
 
 app.all('*', (req, res) => {
 	res.send('404 Not Found')
